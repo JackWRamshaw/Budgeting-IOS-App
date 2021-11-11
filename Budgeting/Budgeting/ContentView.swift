9c1d3c7e7f1fd2ee26039dafd05d5538a23ab1d7
JackWRamshaw <jackram446@gmail.com>
2021-11-11 11:19:15 +0000

    Initial commit
    Basic functionality added.
    To do add dynamic inputs.

---
+++ Budgeting/Budgeting/ContentView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+                .frame(height: 150.0)
+            Text("Spent this month")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            PieChartView(values: [1300, 500, 300], names: ["Rent", "Transport", "Education"], formatter: {value in String(format: "$%.2f", value)})
+                .padding(7.0)
+        }
+        .background(Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0))
+        .edgesIgnoringSafeArea([.top, .bottom])
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
